bec163761ea28f2dab902d53debd31ff2ea594c1
SHTAA <andrew.dunn3@griffithuni.edu.au>
2026-05-25 21:31:45 +1000

    Added: new map

---
+++ Source/UE_Network_Lab/DataIOFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataIOFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_NETWORK_LAB_API UDataIOFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
